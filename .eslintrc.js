67bf1e7b366d1c38fafd6f8721261582e563755e
zhufan <125920969@qq.com>
2018-03-23 18:42:52 +0800

    eslint规则调整，以便使用js的黑魔法

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,11 @@ module.exports = {
   },
   extends: 'airbnb-base',
   plugins: ['import'],
-  rules: {},
+  rules: {
+    'no-underscore-dangle': [0],
+    'no-param-reassign': [0],
+    'prefer-const': [0],
+  },
   globals: {
     // WeixinJSBridge: true,
   },
