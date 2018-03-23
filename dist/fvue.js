7c51546d9611d3630f2c0dd58acfcca426694947
zhufan <125920969@qq.com>
2018-03-23 15:35:51 +0800

    打包生成结果

--- dist/fvue.js
+++ dist/fvue.js
@@ -1,10 +1,41 @@
 var Vue = (function () {
   'use strict';
 
-  const Vue = function Vue() {
-    const a = 'Vue';
-    console.log(`我是${a}`);
-  };
+  /**
+   * 不执行任何操作
+   *
+   * @export
+   */
+  function noop() {}
+
+  /* eslint-disable import/no-mutable-exports */
+  var warn = noop;
+
+  {
+    warn = function warn(msg) {
+      /* eslint-disable no-console */
+      console.error('[Vue warn]:' + msg);
+    };
+  }
+
+  /**
+   * vue构造函数
+   *
+   * @param {any} options 配置选项
+   */
+  function Vue(options) {
+    // "development" 是由rollup进行替换
+    if ("development" !== 'production' && !(this instanceof Vue)) {
+      warn('Vue is a constructor and should be called with the `new` keyword');
+    }
+    console.log(options);
+  }
+
+  // Vue核心入口文件
+
+  Vue.version = '__VERSION__'; // 暂时不知道这个版本是什么意思
+
+  // 全局vue入口文件
 
   return Vue;
 
